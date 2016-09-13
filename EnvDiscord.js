b734ca62f3fe976dc3f25e2bb47bb9c040f0071c
Awkens Ryder <joaofgbraz@gmail.com>
2016-09-13 13:43:35 +0100

    Converted EnvDiscord to a class. Untested.

--- EnvDiscord.js
+++ EnvDiscord.js
@@ -1,241 +1,297 @@
-/* Environment: Discord -- This environment connects to a Discord server/guild. */
-
+// Dependencies
 var jsonfile = require('jsonfile');
 var discord = require('discord.js');
 
-//== Environment settings (discord.env.json)
+var proto = EnvDiscord.prototype;
 
-//*Discord application token
-var token = null;
+/**
+ *  Environment: Discord -- This environment connects to a Discord server/guild.
+ */
+function EnvDiscord(){
+    this._token = null;
+    this._servername = null;
+    this._defaultchannel = null;
+    this._senddelay = 500;
+    this._client = null;
+    this._server = null;
+    this._channels = {};
+    this._outbox = [];
+    this._carrier = null;
+    this._cbError = [];
+    this._cbMessage = [];
+    this._envname = "Discord";
 
-//*Server name to operate on (application must have been previously added to server)
-var servername = null;
-
-//*Default channel to operate on (must be a channel in the above server)
-var defaultchannel = null;
-
-//Send delay (ms)
-var senddelay = 500;
-
-//== 
-
-var client = null;
-var server = null;
-var channels = {};
-var outbox = [];
-var carrier = null;
-
-var cbError = [];
-var cbMessage = [];
-
-
-var envname = "Discord";
-exports.name = envname;
-
-
-exports.initialize = function() {
-    var params = {};
-    try {
-        params = jsonfile.readFileSync("discord.env.json");
-    } catch(e) {}
-    
-    if (params.token) token = params.token;
-    if (!token) return false;
-    
-    if (params.servername) servername = params.servername;
-    if (!servername) return false;
-    
-    if (params.defaultchannel) defaultchannel = params.defaultchannel;
-    if (!defaultchannel) return false;
-    
-    if (params.senddelay) senddelay = params.senddelay;
-    
-    return true;
 }
+var attributes = ["token","servername","defaultchannel","senddelay","client"
+                 ,"server","channels","outbox","carrier","cbError","cbMessage","envname"];
 
-
-exports.connect = function() {
-
-    client = new discord.Client();
-
-    client.on("ready", function(message) {
-        server = client.servers.get("name", servername);
-        channels[server.defaultChannel.name] = server.defaultChannel;
-        if (defaultchannel != server.defaultChannel.name) {
-            channels[defaultchannel] = server.channels.getAll("type", "text").get("name", defaultchannel);
+for(var idx in attributes) {
+    let attr = attributes[idx];
+    Object.defineProperty(proto, attr, {
+        configurable: true,
+        get: function () {
+            return this['_'+attr];
+        },
+        set: function (value) {
+            this['_'+attr] = value;
         }
     });
-    
-    client.on("message", function(message) {
-    
-        if (message.author.username == client.user.username) return;
-        
-        var type = "regular";
-        channelid = message.channel.id;
-        if (message.channel instanceof discord.PMChannel) {
-            type = "private";
-            channelid = message.author.id;
-        }
-        
-        for (var i = 0; i < cbMessage.length; i++) {
-            if (cbMessage[i](envname, type, message.content, message.author.id, channelid, message)) {
-                break;
+}
+//Initializes the environment.
+Object.defineProperty(proto, 'initialize', {
+    configurable: true,
+    value: function () {
+        var params = {};
+        try {
+            params = jsonfile.readFileSync("discord.env.json");
+        } catch(e) {}
+
+        if (params.token) this.token = params.token;
+        if (!token) return false;
+
+        if (params.servername) this.servername = params.servername;
+        if (!servername) return false;
+
+        if (params.defaultchannel) this.defaultchannel = params.defaultchannel;
+        if (!defaultchannel) return false;
+
+        if (params.senddelay) this.senddelay = params.senddelay;
+
+        return true;
+    }
+});
+//Connects to the environment
+Object.defineProperty(proto, 'connect', {
+    configurable: true,
+    value: function () {
+        this.client = new discord.Client();
+
+        this.client.on("ready", function(message) {
+            this.server = this.client.servers.get("name", servername);
+            this.channels[this.server.defaultChannel.name] = this.server.defaultChannel;
+            if (this.defaultchannel != this.server.defaultChannel.name) {
+                this.channels[this.defaultchannel] = this.server.channels.getAll("type", "text").get("name", this.defaultchannel);
             }
-        }
-    });
+        });
 
-    client.loginWithToken(token, genericErrorHandler);
+        this.client.on("message", function(message) {
 
-    carrier = setInterval(deliverMsgs, senddelay);
-}
+            if (message.author.username == this.client.user.username) return;
 
+            var type = "regular";
+            var channelid = message.channel.id;
+            if (message.channel instanceof discord.PMChannel) {
+                type = "private";
+                channelid = message.author.id;
+            }
 
-exports.disconnect = function() {
-    if (carrier) clearInterval(carrier);
-    if (client) client.logout(genericErrorHandler);
-    carrier = null;
-    client = null;
-}
-
-
-exports.msg = function(targetid, msg) {
-
-    var targetchan = null;
-    
-    if (typeof targetid == "string") {
-        if (!channels[targetid]) {
-            channels[targetid] = server.channels.getAll("type", "text").get("id", targetid);
-        }
-        if (!channels[targetid]) {
-            channels[targetid] = server.members.get("id", targetid);
-        }
-        if (!channels[targetid]) {
-            channels[targetid] = server.channels.getAll("type", "text").get("name", targetid);
-        }
-        if (!channels[targetid]) {
-            channels[targetid] = server.members.get("name", targetid);
-        }
-        if (channels[targetid]) {
-            targetchan = channels[targetid];
-        }
-    } else {
-        targetchan = targetid;
-    }
-    
-    if (!targetchan) {
-        targetchan = channels[defaultchannel];
-    }
-    
-    outbox.push([targetchan, msg]);
-}
-
-
-exports.registerOnError = function(func) {  //callback(env, errormsg)
-    cbError.push(func);
-}
-
-
-exports.registerOnMessage = function(func) {  //callback(env, type, message, authorid, rawobject)
-    cbMessage.push(func);
-}
-
-
-exports.idToDisplayName = function(id) {
-    var user = server.members.get("id", id);
-    if (user) {
-        var disp = server.detailsOfUser(user).nick;
-        if (!disp) disp = user.username;
-        return disp;
-    }
-    return id;
-}
-
-    
-exports.displayNameToId = function(displayname) {
-    var refuser = null;
-    
-    var parts = displayname.split("#");
-    if (parts[1]) {
-        refuser = server.members.getAll("username", parts[0]).get("discriminator", parts[1]);
-    } else {
-        var cache = server.members.getAll("username", displayname);
-        if (cache.length == 1) {
-            refuser = cache[0];
-        } else {
-            displayname = displayname.toLowerCase();
-            for (var i = 0; i < server.members.length; i++) {
-                var nick = server.detailsOfUser(server.members[i]).nick;
-                if (nick && nick.toLowerCase() == displayname) {
-                    refuser = server.members[i];
+            for (var i = 0; i < this.cbMessage.length; i++) {
+                if (this.cbMessage[i](this.envname, type, message.content, message.author.id, channelid, message)) {
                     break;
                 }
             }
+        });
+
+        this.client.loginWithToken(token, genericErrorHandler);
+        this.carrier = setInterval(this.deliverMsgs, this.senddelay);
+    }
+});
+// Disconnects from the environment.
+Object.defineProperty(proto, 'initialize', {
+    configurable: true,
+    value: function () {
+        if (this.carrier) clearInterval(this.carrier);
+        if (this.client) this.client.logout(genericErrorHandler);
+        this.carrier = null;
+        this.client = null;
+    }
+});
+// Sends a message
+Object.defineProperty(proto, 'msg', {
+    configurable: true,
+    value: function (targetid, msg) {
+        var targetchan = null;
+
+        if (typeof targetid == "string") {
+            if (!this.channels[targetid]) {
+                this.channels[targetid] = this.server.channels.getAll("type", "text").get("id", targetid);
+            }
+            if (!this.channels[targetid]) {
+                this.channels[targetid] = this.server.members.get("id", targetid);
+            }
+            if (!this.channels[targetid]) {
+                this.channels[targetid] = this.server.channels.getAll("type", "text").get("name", targetid);
+            }
+            if (!this.channels[targetid]) {
+                this.channels[targetid] = this.server.members.get("name", targetid);
+            }
+            if (this.channels[targetid]) {
+                targetchan = this.channels[targetid];
+            }
+        } else {
+            targetchan = targetid;
         }
-    }
-    
-    if (refuser) {
-        return refuser.id;
-    }
-    
-    return null;
-}
 
-
-exports.idIsSecured = function(id) {
-    return true;
-}
-
-
-exports.idIsAuthenticated = function(id) {
-    return true;
-}
-
-
-exports.getRawObject = function() {
-    return {
-        "client": client,
-        "server": server
-    }
-}
-
-
-//Auxiliary
-
-
-function genericErrorHandler(err) {
-    if (!err) return;
-    for (var i = 0; i < cbError.length; i++) {
-        if (cbError[i](envname, err)) {
-            break;
+        if (!targetchan) {
+            targetchan = this.channels[defaultchannel];
         }
+
+        this.outbox.push([targetchan, msg]);
     }
-}
+});
+// Registers an error
+Object.defineProperty(proto, 'registerOnError', {
+    configurable: true,
+    value: function (func) {
+        this.cbError.push(func);
+    }
+});
+// Registers a message
+Object.defineProperty(proto, 'registerOnMessage', {
+    configurable: true,
+    value: function (func) {
+        this.cbMessage.push(func);
+    }
+});
+// Converts id to display Name
+Object.defineProperty(proto, 'idToDisplayName', {
+    configurable: true,
+    value: function (id) {
+        var user = this.server.members.get("id", id);
+        if (user) {
+            var disp = this.server.detailsOfUser(user).nick;
+            if (!disp) disp = user.username;
+            return disp;
+        }
+        return id;
+    }
+});
+// Converts display Name to id
+Object.defineProperty(proto, 'displayNameToId', {
+    configurable: true,
+    value: function (displayname) {
+        var refuser = null;
 
-
-function deliverMsgs() {
-    var packages = {};
-    for (var i = 0; i < outbox.length; i++) {
-        var rawchannelid = outbox[i][0].id;
-        if (!packages[rawchannelid]) {
-            packages[rawchannelid] = {
-                targetchan: outbox[i][0],
-                messages: []
+        var parts = displayname.split("#");
+        if (parts[1]) {
+            refuser = this.server.members.getAll("username", parts[0]).get("discriminator", parts[1]);
+        } else {
+            var cache = this.server.members.getAll("username", displayname);
+            if (cache.length == 1) {
+                refuser = cache[0];
+            } else {
+                displayname = displayname.toLowerCase();
+                for (var i = 0; i < this.server.members.length; i++) {
+                    var nick = this.server.detailsOfUser(this.server.members[i]).nick;
+                    if (nick && nick.toLowerCase() == displayname) {
+                        refuser = this.server.members[i];
+                        break;
+                    }
+                }
             }
         }
-        packages[rawchannelid].messages.push(outbox[i][1]);
+
+        if (refuser) {
+            return refuser.id;
+        }
+
+        return null;
     }
-    for (var rawchannelid in packages) {
-        try {
-            client.sendMessage(
-                packages[rawchannelid].targetchan,
-                packages[rawchannelid].messages.join("\n"),
-                {disableEveryone: true},
-                genericErrorHandler
-            );
-        } catch (e) {
-            genericErrorHandler(e.message);
+});
+// Id is secured
+Object.defineProperty(proto, 'idIsSecured', {
+    configurable: true,
+    value: function (id) {
+        return true;
+    }
+});
+// Id is authenticated
+Object.defineProperty(proto, 'idIsAuthenticated', {
+    configurable: true,
+    value: function (id) {
+        return true;
+    }
+});
+// Raw object
+Object.defineProperty(proto, 'getRawObject', {
+    configurable: true,
+    value: function () {
+        return {
+            "client": this.client,
+            "server": this.server
         }
     }
-    outbox = [];
-}
+});
+// Generic error handler
+Object.defineProperty(proto, 'genericErrorHandler', {
+    configurable: true,
+    value: function (err) {
+        if (!err) return;
+        for (var i = 0; i < this.cbError.length; i++) {
+            if (this.cbError[i](this.envname, err)) {
+                break;
+            }
+        }
+    }
+});
+// Deliver Messages
+Object.defineProperty(proto, 'deliverMsgs', {
+    configurable: true,
+    value: function () {
+        var packages = {};
+        for (var i = 0; i < this.outbox.length; i++) {
+            var rawchannelid = this.outbox[i][0].id;
+            if (!packages[rawchannelid]) {
+                packages[rawchannelid] = {
+                    targetchan: this.outbox[i][0],
+                    messages: []
+                }
+            }
+            packages[rawchannelid].messages.push(this.outbox[i][1]);
+        }
+        for (var rawchannelid in packages) {
+            try {
+                this.client.sendMessage(
+                    packages[rawchannelid].targetchan,
+                    packages[rawchannelid].messages.join("\n"),
+                    {disableEveryone: true},
+                    this.genericErrorHandler
+                );
+            } catch (e) {
+                this.genericErrorHandler(e.message);
+            }
+        }
+        this.outbox = [];
+    }
+});
+
+
+
+//noinspection JSUnresolvedVariable
+module.exports = EnvDiscord;
+
+
+/*
+
+exports.name = envname;
+exports.connect = function() {
+exports.disconnect = function() {
+exports.msg = function(targetid, msg) {
+exports.registerOnError = function(func) {  //callback(env, errormsg)
+exports.registerOnMessage = function(func) {  //callback(env, type, message, authorid, rawobject)
+exports.idToDisplayName = function(id) {
+exports.displayNameToId = function(displayname) {
+exports.idIsSecured = function(id) {
+exports.idIsAuthenticated = function(id) {
+exports.getRawObject = function() {
+function genericErrorHandler(err) {
+function deliverMsgs() {
+*/
+
+
+
+
+
+
+
 
