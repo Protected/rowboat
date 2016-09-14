e28801e5662b68f5bcb008c8d1a275469d914665
Protected <myshelter@gmail.com>
2016-09-14 03:19:30 +0100

    Oops

--- EnvIRC.js
+++ EnvIRC.js
@@ -1,6 +1,6 @@
 /* Environment: IRC -- This environment connects to an IRC server. */
 
-var Environment = require('Environment.js');
+var Environment = require('Environment');
 var irc = require('irc');
 
 class EnvIRC extends Environment {
