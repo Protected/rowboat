86dbf853ed13f791f26805e6db14ac99708b310e
Protected <myshelter@gmail.com>
2016-09-19 12:19:34 +0100

    Untested implementation of ModActivity.

--- ModActivity.js
+++ ModActivity.js
@@ -11,7 +11,8 @@ class ModActivity extends Module {
     get optionalParams() { return [
         'datafile',
         'permissionSeen',
-        'permissionLast'
+        'permissionLast',
+        'linesPerUser'
     ]; }
     
     get requiredModules() { return [
@@ -23,8 +24,17 @@ class ModActivity extends Module {
         super('Activity', name);
         
         this._params['datafile'] = 'users.data.json';
+        this._params['linesPerUser'] = 5;
         
+        //Main map: {ENV => {NICKNAME => {REGISTER}, ...}, ...}
         this._activitydata = {};
+        
+        //Indices
+        this._authorseen = {};
+        this._authorspoke = {};
+        
+        //Timer
+        this._activitysaver = null;
     }
 
 
@@ -35,13 +45,108 @@ class ModActivity extends Module {
         
         if (!this.loadActivity()) return false;
         
-        //TODO Save timer
-        
+        var self = this;
+        this._activitysaver = setInterval(() => {
+            self.saveActivity();
+        }, 60000);
+
 
         //Register callbacks
         
-        //TODO Listeners
-        //TODO Commands
+        for (var envname in envs) {
+            envs[envname].registerOnJoin(this.onJoin, this);
+            envs[envname].registerOnPart(this.onPart, this);
+            envs[envname].registerOnMessage(this.onMessage, this);
+        }
+
+        
+        this.mod("Commands").registerCommand('seen', {
+            description: "Reports when a user was last seen (or if the user was never seen).",
+            args: ["nickname", "environment"],
+            minArgs: 1,
+            permissions: (this.param('permissionSeen') ? [this.param('permissionSeen')] : null)
+        }, (env, type, userid, command, args, handle, reply) => {
+            
+            var envname = args.environment;
+            if (!envname) envname = env.name;
+            
+            var register = null;
+            
+            if (args.nickname[0] == "=") {
+                //Lookup by handle
+            
+                for (let id in this._authorseen[envname]) {
+                    if (this.mod("Users").isIdHandle(args.nickname.substr(1), envname, id)) {
+                        if (!register || this._authorseen[envname][id].seen[2] > register.seen[2]) {
+                            register = this._authorseen[envname][id];
+                        }
+                    }
+                }
+                
+            } else {
+                //Lookup by nickname
+                
+                let check = this._activitydata[envname][args[0].nickname.toLowerCase()];
+                if (check) register = check;
+                
+            }
+            
+            if (!register) {
+                reply("I have never seen " + args.nickname + " in " + envname + ".");
+            } else if (register.seenreason === true) {
+                reply(args.nickname + " joined " + register.seen[0] + " " + moment(register.seen[2]).fromNow() + ".");
+            } else if (register.seenreason === false) {
+                reply(args.nickname + " talked in " + register.seen[0] + " " + moment(register.seen[2]).fromNow() + ".");
+            } else {
+                reply(args.nickname + " left " + register.seen[0] + " " + moment(register.seen[2]).fromNow() + " (reason: " + register.seenreason + ").");
+            }
+            
+        });
+        
+        
+        this.mod("Commands").registerCommand('last', {
+            description: "Repeats a user's latest chat lines in record.",
+            args: ["nickname", "environment"],
+            minArgs: 1,
+            permissions: (this.param('permissionLast') ? [this.param('permissionLast')] : null)
+        }, (env, type, userid, command, args, handle, reply) => {
+        
+            var envname = args.environment;
+            if (!envname) envname = env.name;
+            
+            var entries = [];
+            
+            if (args.nickname[0] == "=") {
+                //Lookup by handle
+            
+                for (let id in this._authorspoke[envname]) {
+                    if (this.mod("Users").isIdHandle(args.nickname.substr(1), envname, id)) {
+                        for (let entry of this._authorspoke[envname][id]) {
+                            entries.push(entry);
+                        }
+                    }
+                }
+            
+            } else {
+                //Lookup by nickname
+                
+                let check = this._activitydata[envname][args[0].nickname.toLowerCase()];
+                if (check) entries = check.last;
+                
+            }
+            
+            entries = entries.slice(-1 * this.param('linesPerUser'));
+            
+            if (!entries.length) {
+                reply("I have never read anything by " + args.nickname + " in " + envname + ".");
+            } else {
+                reply('Latest lines of ' + nickname + ' in ' + envname + ':');
+                for (entry of entries) {
+                    reply('[' + entry[0] + '] (' + moment(entry[2]).format('dddd YYYY-MM-DD HH:mm:ss') + ') ' + entry[3]);
+                }
+            }
+            
+        });
         
         return true;
     }
@@ -68,6 +173,16 @@ class ModActivity extends Module {
         }
         if (!this._activitydata) this._activitydata = {};
         
+        //Build indices
+        
+        for (let envname in this._activitydata) {
+            for (let nicknamelc in this._activitydata[envname]) {
+                let register = this._activitydata[envname][nicknamelc];
+                this.authorSeen(envname, register);
+                this.authorSpoke(envname, register);
+            }
+        }
+        
         return true;
     }
 
@@ -78,9 +193,94 @@ class ModActivity extends Module {
     }
     
     
+    getNickRegister(env, nickname) {
+        var envregister = this._activitydata[env];
+        if (!envregister) envregister = this._activitydata[env] = {};
+        
+        var nickregister = envregister[nickname.toLowerCase()];
+        if (!nickregister) nickregister = envregister[nickname.toLowerCase()] = {
+            nickname: nickname,
+            seen: null,  //[channelid, authorid, timestamp]
+            seenreason: null,
+            last: []  //Each entry: [channelid, authorid, timestamp, message]
+        };
+        
+        return nickregister;
+    }
+    
+    
+    //Helpers for indexing registers by author (used only in runtime)
+    
+    authorSeen(env, register) {
+        if (!register || !register.seen) return false;
+        let authorid = register.seen[1];
+        
+        var envindex = this._authorseen[env];
+        if (!envindex) envindex = this._authorseen[env] = {};
+        
+        if (!envindex[authorid] || register.seen[2] > envindex[authorid].seen[2]) {
+            envindex[authorid] = register;
+        }
+        return true;
+    }
+    
+    authorSpoke(env, register) {
+        if (!register || !register.last || !register.last.length) return false;
+        
+        var envindex = this._authorspoke[env];
+        if (!envindex) envindex = this._authorspoke[env] = {};
+        
+        for (let entry of register.last) {
+            let authorid = entry[1];
+            if (!envindex[authorid]) envindex[authorid] = [];
+            envindex[authorid].push(entry);
+        }
+        return true;
+    }
+    
+    
     //Event handlers
     
-    //TODO
+    onJoin(env, authorid, channelid, rawobj) {
+        var nickname = env.idToDisplayName(authorid);
+        var register = this.getNickRegister(env.name, nickname);
+        
+        register.seen = [channelid, authorid, moment().unix()];
+        register.seenreason = true;
+        
+        this.authorSeen(env.name, register);
+    }
+    
+    
+    onPart(env, authorid, channelid, reason, rawobj) {
+        var nickname = env.idToDisplayName(authorid);
+        var register = this.getNickRegister(env.name, nickname);
+        
+        register.seen = [channelid, authorid, moment().unix()];
+        register.seenreason = reason;
+        
+        this.authorSeen(env.name, register);
+    }
+    
+    
+    onMessage(env, type, message, authorid, channelid, rawobj) {
+        var nickname = env.idToDisplayName(authorid);
+        var register = this.getNickRegister(env.name, nickname);
+        var ts = moment().unix();
+        
+        register.seen = [channelid, authorid, ts];
+        register.seenreason = false;
+        
+        this.authorSeen(env.name, register);
+        
+        register.last.push([channelid, authorid, ts, message]);
+        while (register.last.length > this.param('linesPerUser')) {
+            register.last.shift();
+        }
+        
+        this.authorSpoke(env.name, register);
+    }
+    
     
 }
 
