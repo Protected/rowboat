cce1c2d41016ed5be6c760a6f3e214b4de8c9998
Protected <myshelter@gmail.com>
2017-01-30 21:37:53 +0000

    More dice crap

--- ModRandom.js
+++ ModRandom.js
@@ -24,8 +24,7 @@ class ModRandom extends Module {
             args: ["max", "pub"],
             details: [
                 "If MAX is not passed, a floating point number between 0 and 1 is returned.",
-                "If MAX Is a positive integer, an integer between 0 and MAX is returned.",
-                "If MAX is a dice descriptor with the format AdB, where A and B are integers, a roll of A dice with B facets is realistically simulated and the value returned. Up to 9 dice and 99 facets are accepted.",
+                "If MAX is a positive integer, an integer between 0 and MAX is returned.",
                 "Pass PUB as 1 to display the resulting random number in public even if you used the command in private."
             ],
             minArgs: 0
@@ -34,13 +33,7 @@ class ModRandom extends Module {
             var val;
         
             if (args.max) {
-                let facets = args.max.match(/^([1-9])d([1-9][0-9]?)$/);
-                if (facets) {
-                    val = 0;
-                    for (let i = 0; i < facets[1]; i++) {
-                        val += Math.floor(random.fraction() * facets[2]) + 1;
-                    }
-                } else if (args.max.match(/^[0-9]+$/)) {
+                if (args.max.match(/^[0-9]+$/)) {
                     val = Math.floor(random.fraction() * args.max);
                 } else {
                     ep.reply("Invalid argument.");
@@ -58,6 +51,66 @@ class ModRandom extends Module {
         
             return true;
         });
+        
+        
+        this.mod('Commands').registerCommand(this, 'roll', {
+            description: "Generates a dice roll using a cryptographically secure source of randomness.",
+            args: ["expr", true],
+            details: [
+                "EXPR is an expression containing sums and subtractions between dice (in the format AdB where A and B are integers) and constants."
+            ],
+            minArgs: 1
+        }, (env, type, userid, channelid, command, args, handle, ep) => {
+        
+            var expr = args.expr.join(" ").replace(/(\+ *-|- *\+)/g, '-').replace(/([0-9])([+-])/g, '$1 $2').replace(/([+-])([0-9])/g, '$1 $2').replace(/ +/g, " ").split(" ");
+            for (let i = 1; i < expr.length; i += 2) {
+                if (expr[i] != "-" && expr[i] != "+") {
+                    ep.reply("Invalid token '" + expr[i] + "' on position " + i + ".");
+                    return true;
+                }
+            }
+        
+            var dicepos = {};
+            var resolved = expr.slice();
+            for (let i = 0; i < resolved.length; i++) {
+                let facets = resolved[i].match(/^([1-9][0-9]?)?d([1-9][0-9]?)$/);
+                if (facets) {
+                    let val = 0;
+                    for (let j = 0; j < facets[1]; j++) {
+                        val += Math.floor(random.fraction() * facets[2]) + 1;
+                    }
+                    resolved[i] = val;
+                    dicepos[i] = true;
+                }
+            }
+
+            var rep = expr.join(" ");
+            
+            if (Object.keys(dicepos).length > 0) {
+                rep += ' = ' + resolved.map((val, i) => {
+                    if (dicepos[i]) {
+                        return "__" + val + "__";
+                    }
+                }).join(" ");
+            }
+            
+            if (resolved.length > 1) {
+                let result = resolved[0];
+                for (let i = 2; i < resolved.length; i++) {
+                    if (resolved[i-1] == "+") {
+                        result += resolved[i];
+                    } else if (resolved[i-1] == "-") {
+                        result -= resolved[i];
+                    }
+                }
+                rep += ' = **' + result;
+            }
+
+            ep.reply(env.idToDisplayName(userid) + ': ' + rep);
+        
+            return true;
+        });
+        
       
         return true;
     };
